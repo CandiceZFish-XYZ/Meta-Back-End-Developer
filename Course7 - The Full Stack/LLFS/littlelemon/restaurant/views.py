e2779179eaad271700476e3549943dd244eb4d23
CandiceZFish <daintycandy@gmail.com>
2023-05-17 11:49:41 -0700

    display booking times

--- Course7 - The Full Stack/LLFS/littlelemon/restaurant/views.py
+++ Course7 - The Full Stack/LLFS/littlelemon/restaurant/views.py
@@ -7,6 +7,7 @@ from .models import Booking
 from datetime import datetime
 import json
 from .forms import BookingForm
+from django.views.decorators.csrf import csrf_exempt
 
 def home(request):
     return render(request, 'index.html')
@@ -24,14 +25,6 @@ def book(request):
     context = {'form':form}
     return render(request, 'book.html', context)
 
-# Add code for the bookings() view
-def bookings(request):
-    data = request.GET.get('date', datetime.today().date())
-    bookings = Booking.objects.using('reservations').all()
-    booking_json = serializers.serialize('json', bookings)
-    return render(request, 'bookings.html', {'bookings': booking_json})
-
-
 
 def menu(request):
     menu_data = Menu.objects.using('reservations').all()
@@ -45,3 +38,24 @@ def display_menu_item(request, pk=None):
     else: 
         menu_item = "" 
     return render(request, 'menu_item.html', {"menu_item": menu_item}) 
+
+@csrf_exempt
+def bookings(request):
+    if request.method == 'POST':
+        data = json.load(request)
+        exist = Booking.objects.using('reservations').filter(reservation_date=data['reservation_date']).filter(
+            reservation_slot=data['reservation_slot']).exists()
+        if exist==False:
+            booking = Booking.objects.using('reservations').create(
+                first_name=data['first_name'],
+                reservation_date=data['reservation_date'],
+                reservation_slot=data['reservation_slot'],
+            )
+        else:
+            return HttpResponse("{'error':1}", content_type='application/json')
+    
+    date = request.GET.get('date',datetime.today().date())
+    bookings = Booking.objects.using('reservations').all().filter(reservation_date=date)
+    booking_json = serializers.serialize('json', bookings)
+    
+    return HttpResponse(booking_json, content_type='application/json')
